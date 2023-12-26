69c0c07e907637ab4c6006df91a370471068054d
Nesb01t <nesb01t@163.com>
2023-12-26 22:36:09 +0800

    update player position

--- Assets/Scripts/Player/MouseMove.cs
+++ Assets/Scripts/Player/MouseMove.cs
@@ -3,16 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseMove : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{  
+    public Animator animator;
+    
 
-    // Update is called once per frame
+    private void AnimatorTrigger()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetTrigger("Move");
+        }
+        else
+        {
+            animator.SetTrigger("Stop");
+        }
+    }
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = GetClickPos();
+            MoveTo(pos);
+        }
+        AnimatorTrigger();
+    }
+    
+    private void MoveTo(Vector3 pos)
+    { 
+        // move
+        Vector3 v = pos - transform.position;
+        v.y = 0;
+        transform.position += v.normalized * Time.deltaTime * 4.5f ;
+
+        // face
+        transform.forward = v;
+    }
+
+    private Vector3 GetClickPos()
+    {  
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.point);
+            return hit.point;
+        }
+        return Vector3.zero;
     }
 }
