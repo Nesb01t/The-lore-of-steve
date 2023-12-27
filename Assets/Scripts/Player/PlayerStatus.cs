1c4cac546fb50b5f536d363f0ab90f4669d9c592
Nesb01t <nesb01t@163.com>
2023-12-27 20:26:22 +0800

    update editor themes

---
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    private float _health = 20.0f;
+
+    private float _mana = 20.0f;
+    
+    void Start()
+    {
+        
+    }
+ 
+    void Update()
+    {
+        
+    }
+}
